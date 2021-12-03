19e096388f15daf8d4f114286b3234bbbc0c15e4
TavionBritt <tavion.britt99@gmail.com>
2021-12-03 17:52:02 -0500

    Finished part one of day 3

--- 3/main.java
+++ 3/main.java
@@ -5,47 +5,68 @@ import java.io.FileNotFoundException;
 
 public class main{
 	public static void main(String[] args){
-		ArrayList<Integer> gamma = new ArrayList<Integer>();
-		ArrayList<Integer> epsilon = new ArrayList<Integer>();
-		ArrayList<Integer> lines = new ArrayList<Integer>();
+		ArrayList<String> gamma = new ArrayList<String>();
+		ArrayList<String> epsilon = new ArrayList<String>();
+		ArrayList<String> lines = new ArrayList<String>();
 
 		try{
 			File f = new File(args[0]);
 			Scanner scan = new Scanner(f);
-			while(scan.hasNextInt()){
-				int test = scan.nextInt();
-				System.out.println(test);
+			while(scan.hasNextLine()){
+				String line = scan.nextLine();
+				lines.add(line);
 			}
-			while(scan.hasNextInt()){
-				int t = scan.nextInt();
-				System.out.println(t);
-				lines.add(t);
-			}
-			System.out.println(lines);
 		}catch(FileNotFoundException e){
 			System.out.println("File not found");
 			System.exit(0);
 		}
 		//get length of a line
-		int len = 12;
+		int len = lines.get(0).length();
 		for(int x =0;x<len;x++){
 			int ones = 0;
 			int zeros = 0;
-			for(int y = 0; y < lines.size(); y++){
-				int temp = Integer.parseInt(String.valueOf(String.valueOf(lines.get(y)).charAt(x)));
-				if(temp == 0){
+			//System.out.println("position " + x);
+			for(int y = 0; y < lines.size()-1; y++){
+				//get first char
+				char c = lines.get(y).charAt(x);
+				//turn char into int
+				int temp = Character.getNumericValue(c);
+				if(temp > 0){
 					ones++;
 				}
 				else{
 					zeros++;
 				}
+				// System.out.println(ones);
+				// System.out.println(zeros);
 			}
 			if(ones>zeros){
-				System.out.println("more ones");
+				gamma.add("1");
+				epsilon.add("0");
 			}
 			else{
-				System.out.println("more zeros");
+				gamma.add("0");
+				epsilon.add("1");
 			}
 		}
+		String g = "";
+		String e = "";
+		for(String s : gamma){
+			
+			g+= s;
+		}
+		for(String s : epsilon){
+			e+= s;
+		}
+		System.out.println(g);
+		System.out.println(e);
+		int ga = Integer.parseInt(g,2);
+		int ep = Integer.parseInt(e,2);
+		System.out.println(ga);
+		System.out.println(ep);
+		int ans = ga * ep;
+		System.out.println(ans);
+
+		//Oxygen generator and the other thing
 	}
 }
